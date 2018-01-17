be77f1d8b19c38a75d2ed1261f7205ba627be900
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-16 18:38:33 -0800

    a

--- hello.c
+++ hello.c
@@ -2,7 +2,22 @@
 
 int main()
 {
-	printf("Hello, world\n");
+	
+	r = 9;
+	int topArray[r] = {theFirst, quick, brown, fox, jumps, over, the, lazy, dog};
+
+	char theFirst[] = {'T', 'h', 'e'};
+	char quick[] = {'q','u','i','c','k'};
+	char brown[] = {'b','r','o','w','n'};
+	char fox[] = {'f','o', 'x'};
+	char jumps[] = {'j','u','m','p','s'}
+	char over[] = {'o', 'v', 'e', 'r'}
+	char theSecond[] = {'t', 'h', 'e'}
+	char lazy[] = {'l','a','z', 'y'}
+	char dog[] = {'d','o', 'g'}
+
+	printf("%c", theFirst)
+
 
 	return 0;
 }
