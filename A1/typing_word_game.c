f19042150c6e2bc257e258352749536ae3890e44
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-19 14:28:46 -0800

    a

--- A1/typing_word_game.c
+++ A1/typing_word_game.c
@@ -15,86 +15,38 @@
 #include <time.h>
 #include <string.h>
 
-int * permutationBank(int numberBank[]);
-int numberChecker(int tempNumber, int numberBank[]);
-void printWords(char *topArray[],int numberBank[]);
+int fisherYatesShuffle(char * words[]);
+void printing(char * words[]);
 
 int main()
 {
 	srand((unsigned)time(NULL));
-
-	int numberBank[9];
-
-	char theFirst[] = {'T', 'h', 'e'};
-	char quick[] = {'q','u','i','c','k'};
-	char brown[] = {'b','r','o','w','n'};
-	char fox[] = {'f','o', 'x'};
-	char jumps[] = {'j','u','m','p','s'};
-	char over[] = {'o', 'v', 'e', 'r'};
-	char theSecond[] = {'t', 'h', 'e'};
-	char lazy[] = {'l','a','z', 'y'};
-	char dog[] = {'d','o','g'};
-
-	char *topArray[] = {theFirst, quick, brown, fox, jumps, over, theSecond, lazy, dog}; 
-	printf("Before Permutation Bank\n");
-	//printf("Printing: %s \n", topArray[0]);
-	printWords(topArray, permutationBank(numberBank));
+	char * words[] ={"The", "quick", "brown","fox", "jumps", "over", "the", "lazy", "dog"};
+	words[] = fisherYatesShuffle(words);
+	printing(words);
 
 exit;
 }
 
-
-void printWords(char *topArray[], int numberBank[])
-{	
+int fisherYatesShuffle(char * words[])
+{
 	int i;
 	int j;
-	int temp;
-	int size;
+	j = ((rand() % 9) - i);
+	char * tmp = words[];
 	for(i = 0; i <= 8; i++)
 	{
-		printf("Inside forloop i\n");
-		temp = numberBank[i];
-		size = strlen(topArray[temp]);
-		for(j = 0; j <= size; j++)
-		{
-			printf("Inside forloop %d for len %d\n", j, 8);
-			printf("%c", topArray[temp][j]);
-		}
+		char * tmp = words[i];
+		words[i] = words[j];
+		words[j] = tmp;
 	}
-	printf("\n");
+	return words;
 }
 
-int * permutationBank(int numberBank[])
-{ 
-	int k;
-	int i = 0;
-	while(i <= 8)
-	{	
-		//printf("Inside permutationBank\n");
-		int tempNumber = ((rand() % 9)+1);
-
-		if(numberChecker(tempNumber, numberBank))
-		{
-			numberBank[i] = tempNumber;
-			i++;				
-		}
-
-	}
-	//printf("Coming out of permutationBank\n");
-	return numberBank;
-}
-
-int numberChecker(int tempNumber, int numberBank[])
+void printing(char * words[])
 {
-	int j;
-	int k;
-	//printf("Inside numberChecker\n");
-	for(j = 0; j <= 8 ; j++)
-	{ 
-		if (tempNumber == numberBank[j])
-		{
-			return 0;
-		}
-	}
-	return 1;
+	int n;
+	for(n = 0; n <= 8; n++)
+		printf("%s\n", words[n]);
 }
+
