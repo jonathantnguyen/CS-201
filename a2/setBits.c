c70de893f39f3b4512102ff8e8b0e4b783aa9b06
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-22 13:00:45 -0800

    a

--- a2/setBits.c
+++ a2/setBits.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+
 
 int main(int argc, char **argv)
 {
@@ -8,9 +10,14 @@ int main(int argc, char **argv)
 
 	for(long i = strtol(*argv, &end, 2); *argv != end; i = strtol(*argv, &end, 2))
 	{
-		printf("%ld\n", i);
+		*argv = end;
+		if(errno == ERANGE)
+		{
+		printf("Range Error.\n");
+		errno = 0;
+		}
+	printf("%ld\n", i);
 	}
-
 
 exit;
 }
