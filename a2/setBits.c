c1488eca191f2ea7769212201aa6e030965bdcdb
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-22 12:50:04 -0800

    a

--- a2/setBits.c
+++ a2/setBits.c
@@ -5,12 +5,8 @@ int main(int argc, char **argv)
 {
 	int i;
 	char *end;
-	if (argc == 0)
-	{
-		return 0;
-	}
 
-	for(long i = strtol(*argv, &end, 2); *argv != end; i = strtol(*argv, &end, 2))
+	for(long i = strtol(argv, &end, 2); argv != end; i = strtol(argv, &end, 2))
 	{
 		printf("%ld\n", i);
 	}
