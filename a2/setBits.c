51f9e9fa4469f86eac44c3f5d53b9c6667401208
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-22 13:55:55 -0800

    a

--- a2/setBits.c
+++ a2/setBits.c
@@ -17,7 +17,7 @@ int main(int argc, char **argv)
 
 	for (int j = 1; j < argc; j++)
 	{	
-		* array = strtol( * argv, &end, 10)
+		* array = strtol( * argv, &end, 10);
 		printf("%li\n", (long) strtol(argv[j], &end, 10));
 	}
 	/*
