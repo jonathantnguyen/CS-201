f6fce8a9b5bcee9250e79c011abf6b7de4386b93
jonathantnguyen <jonnytnguyen@gmail.com>
2018-01-30 18:38:07 -0800

    a

--- Mini Programs/Midterm Practice Q1.c
+++ Mini Programs/Midterm Practice Q1.c
@@ -6,15 +6,15 @@ int main()
 	int i = -100;
 
 	if (i == (int)(float) i)
-		printf("true");
+		printf("true %f\n", i);
 	else 
-		printf("false");
+		printf("false %f\n", i);
 
 
 	if (i == (int)(double) i)
-		printf("true");
+		printf("true %f\n", i);
 	else 
-		printf("false");
+		printf("false %f\n", i);
 
 	exit;
 }
