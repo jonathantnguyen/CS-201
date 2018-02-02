91f80024110d7115e9d894c18be72a507cde802b
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-02 11:19:56 -0800

    a

--- a2/showBits.c
+++ a2/showBits.c
@@ -2,41 +2,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-int check(int argc, char ** argv);
+void check(int argc, char ** argv);
 int hToD(char ** argv);
 
 int main(int argc, char ** argv)
 {
 	check(argc, argv);
+	//hToD(argc, argv);
 	exit; 
 }
 
-int check(int argc, char ** argv)
+void check(int argc, char ** argv)
 {
 	if(argc == 1)
 	{
 		printf("No arguments, execute with arguments.\n");
-		return 0;
+		exit;
 	}
-
-	hToD(argc, argv);
-}
-
-int hToB(int argc, char ** argv)
-{	
-	int hexArray[];
-	char * end;
-	for (int j = 1; j < argc; j++)
-	{	
-		*argv = end;
-		if (isalpha(argv[j]))
-		{
-			alphToB(argv[j])
-		}
-	}
-}
-
-int alphaToB(char ** argv)
-{
-	
 }
