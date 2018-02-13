34b7ce7708d9498731533f64d5a2cfd4faa0d1d7
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-12 20:21:22 -0800

    a

--- Mini Programs/double for loop.c
+++ Mini Programs/double for loop.c
@@ -5,11 +5,13 @@ int main(){
 	int n = 9;
 	int l = 0;
 
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++){
 		printf("%i\n", i );
-		for(int j = i+1; j<=n; j++)
+	}
+		for(int j = i+1; j<=n; j++){
 			printf("%i\n", j);
 			l += 1;
 		printf("%i\n", l );
+		}
 exit;
 }
