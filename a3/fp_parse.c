cb301954b467cb91205527aa73430de424626f7e
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-13 17:15:12 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -8,20 +8,26 @@ int main(int argc, int ** argv)
 }
 void check(int argc, int ** argv)
 {
+	int flag_error = 0;
+
 	if(argc != 4)
 	{
+		flag_error = true;
 		printf("<# of frac_bits> <# of exp_bits> <hex_to_convert>\n");
-		exit(0);
 	}
 
 	if((argv[1] <= 2) || (argv[1] >= 10))
 	{
+		flag_error = true;
 		printf("Number of fraction bits between 2 and 10\n");
-		exit(0);
+
 	}
 	if((argv[2] <= 3) || (argv[2] >=5))
 	{
+		flag_error = true;
 		printf("Number of exponent bits (k) between 3 and 5\n");
-		exit(0);
 	}
+
+	if(flag_error == true)
+		exit(0);
 }
