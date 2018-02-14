8970d458ea62f85d7527046d48ff6fbf274f2e0d
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-13 17:40:33 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -23,23 +23,22 @@ void strtol_func(int argc, char ** argv)
 void check(int argc, char ** argv)
 {
 	int flag_error = 0;
-	true = 1;
 
 	if(argc != 4)
 	{
-		flag_error = true;
+		flag_error = 1;
 		printf("<# of frac_bits> <# of exp_bits> <hex_to_convert>\n");
 	}
 
 	if(( *argv[1] <= 2) || ( *argv[1] >= 10))
 	{
-		flag_error = true;
+		flag_error = 1;
 		printf("Number of fraction bits between 2 and 10\n");
 
 	}
 	if(( *argv[2] <= 3) || ( *argv[2] >=5))
 	{
-		flag_error = true;
+		flag_error = 1;
 		printf("Number of exponent bits (k) between 3 and 5\n");
 	}
 
