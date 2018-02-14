e8e33722d1a075b7250cdaa2a39a09757884b8ec
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-13 18:15:46 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -6,9 +6,7 @@ void strtol_func(int argc, char ** argv);
 
 int main(int argc, char ** argv)
 {
-	printf("segfault 0\n");
 	strtol_func(argc, argv);
-	printf("segfault end\n");
 	//check(argc, argv);
 exit(0);
 }
@@ -20,9 +18,8 @@ void strtol_func(int argc, char ** argv)
 	for (int i = 1; i < 4; i++)
 	{
 		*argv = end;
-		printf("segfault 1\n");
+
 		*argv[i] = (unsigned long) strtol(argv[i], &end, 10);
-		printf("segfault 2\n");
 		printf("Value: %i \n", *argv[i]);
 
 	}
