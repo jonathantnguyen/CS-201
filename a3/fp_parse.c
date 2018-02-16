60543911861350a92339c30fc1a887bcd361f9d4
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-16 12:43:03 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -3,9 +3,9 @@
 #include <stdint.h>
 
 
-void check(int frac, int exp);
-void strtol_func(int frac, int exp, char const *argv[]);
-void bit_manip(int frac, int exp, char const * argv[]);
+void check(uint32_t frac, uint32_t exp);
+void strtol_func(uint32_t frac, uint32_t exp, char const *argv[]);
+void bit_manip(uint32_t frac, uint32_t exp, char const * argv[]);
 
 int flag_error = 0;
 
@@ -21,8 +21,8 @@ int flag_error = 0;
 
 int main(int argc, char const *argv[])
 {
-	int frac;
-	int exp;
+	uint32_t frac;
+	uint32_t exp;
 	printf("top of int main\n");
 	if(argc != 4)
 	{
@@ -36,7 +36,7 @@ int main(int argc, char const *argv[])
 	exit(0);
 }
 
-void bit_manip(int frac, int exp, char const*argv[])
+void bit_manip(uint32_t frac, uint32_t exp, char const*argv[])
 {
 	int sign = 0;
 
@@ -63,7 +63,7 @@ void bit_manip(int frac, int exp, char const*argv[])
  * @param[in]  argc  argument count
  * @param      argv  argument values
  */
-void strtol_func(int frac, int exp, char const *argv[])
+void strtol_func(uint32_t frac, uint32_t exp, char const *argv[])
 {
 	char * end;
 
@@ -86,7 +86,7 @@ void strtol_func(int frac, int exp, char const *argv[])
  * @param[in]  argc  argument count
  * @param      argv  argument values
  */
-void check(int frac, int exp)
+void check(uint32_t frac, uint32_t exp)
 {
 
 	if(( frac < 2) || ( frac > 10))
