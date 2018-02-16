e76b9bda400e448da9f4f62072e4e55bc08c8efc
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-15 18:24:29 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -39,10 +39,11 @@ int main(int argc, char const *argv[])
 void bit_manip(int frac, int exp, char const*argv[])
 {
 	int sign = 0;
+	uint32_t mask = 1;
 	uint32_t temp_hex = *argv[3];
 	printf("%u\n",temp_hex);
 	printf("before mask value%u\n",mask );
-	uint32_t mask = 1 << (frac + exp + 1);
+	mask = mask << (frac + exp + 1);
 	printf("after mask value%u\n",mask );
 	sign = (temp_hex & mask) ? 1 : 0;
 	printf("%u\n",temp_hex);
