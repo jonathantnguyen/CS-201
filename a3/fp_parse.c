ca2c4ac8cab530c651bddcf6610ac2f21d53aaee
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-15 18:05:26 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -5,7 +5,7 @@
 
 void check(int argc, char const *argv[]);
 int strtol_func(int frac, int exp, char const *argv[]);
-void bit_manip(int ** array);
+void bit_manip(int frac, int exp);
 
 int flag_error = 0;
 
