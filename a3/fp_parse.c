47c212a795e14acb81c7e90f3b6bd004350e2295
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-18 12:33:15 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -61,9 +61,9 @@ void fp_funct()
 	float fp_value = 0;
 	int m_value = 0;
 
-	temp_exp_value = temp_exp_value << (16 - (FRAC_BIT + EXP_BIT));
+	temp_exp_value = temp_exp_value << (16 - (FRAC_BIT + EXP_BIT + 1));
 	printf("FRAC_BIT: %i, EXP_BIT: %i, HEXDECIMAL: %i\n",FRAC_BIT,EXP_BIT, temp_exp_value);
-	temp_exp_value = temp_exp_value >> (16 - (FRAC_BIT + EXP_BIT));
+	temp_exp_value = temp_exp_value >> (16 - (FRAC_BIT + EXP_BIT + 1));
 	printf("FRAC_BIT: %i, EXP_BIT: %i, HEXDECIMAL: %i\n",FRAC_BIT,EXP_BIT, temp_exp_value);
 
 	if (temp_exp_value == 0) // DENORMALIZED
