3134e4a8a2598a2db4b2f96dadeded713f847d9f
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-18 12:27:31 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -65,7 +65,7 @@ void fp_funct()
 	temp_exp_value = temp_exp_value >> (16 - (FRAC_BIT + EXP_BIT));
 	printf("FRAC_BIT: %i, EXP_BIT: %i, HEXDECIMAL: %i\n",FRAC_BIT,EXP_BIT, temp_exp_value);
 
-	if (temp_exp_value == 0); // DENORMALIZED
+	if (temp_exp_value == 0) // DENORMALIZED
 	{
 		e_value = 1 - BIAS;
 		
