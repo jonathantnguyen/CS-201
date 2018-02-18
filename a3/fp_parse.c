f688f2d46ae5dc23b999c968a197497ec9e5acb2
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-18 12:32:19 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -62,6 +62,7 @@ void fp_funct()
 	int m_value = 0;
 
 	temp_exp_value = temp_exp_value << (16 - (FRAC_BIT + EXP_BIT));
+	printf("FRAC_BIT: %i, EXP_BIT: %i, HEXDECIMAL: %i\n",FRAC_BIT,EXP_BIT, temp_exp_value);
 	temp_exp_value = temp_exp_value >> (16 - (FRAC_BIT + EXP_BIT));
 	printf("FRAC_BIT: %i, EXP_BIT: %i, HEXDECIMAL: %i\n",FRAC_BIT,EXP_BIT, temp_exp_value);
 
