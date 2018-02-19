d245c37e01b208ff547b20dd21e60b172baa4c2a
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-18 19:22:16 -0800

    a

--- a3/fp_parse.c
+++ a3/fp_parse.c
@@ -67,6 +67,7 @@ void fp_funct()
 	{
 		printf("DENORMALIZED\n");
 		e_value = pow(2,1 - BIAS);
+		printf("e_value:  %f\n",e_value);
 		for (int i = EXP_BIT; i > 0; i--)	
 		{	
 			if (temp_frac_value & 1)
