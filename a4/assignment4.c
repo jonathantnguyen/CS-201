379306f93f11dd7af57064e2aa776d789a68d3c8
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-27 18:21:12 -0800

    a

--- a4/assignment4.c
+++ a4/assignment4.c
@@ -24,10 +24,10 @@ int _menu()
 	printf("Please enter one of the following options by it's corresponding number, and press ENTER\n");
 	printf("\n 0. Exit\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Module\n 6. Reverse Input\n\n");
 
-	scanf(" %d\n", &choice);
-	if (!isalpha(choice))
+	scanf(" %i\n" &choice)
+	if(isalpha(choice))
 	{
-		choice = -1;
+		_menu();
 	}
 
 	if ((choice < 0) || (choice > 6))
