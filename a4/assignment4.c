78a42c4bfd5264e34f435fec42a0c0157a260210
jonathantnguyen <jonnytnguyen@gmail.com>
2018-02-27 17:44:04 -0800

    a

---
+++ a4/assignment4.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+void print_menu();
+
+int main(int argc, char const *argv[])
+{
+	print_menu();
+
+
+
+	exit(0);
+
+}
+
+void print_menu()
+{
+	printf("Please enter one of the following options by it's corresponding number, and press ENTER\n");
+	printf("     0. Exit\n     1. Addition\n     2.Subtraction\n     3.Multiplication\n
+	     4.Division\n     5.Module\n     6.Reverse Input\n");
+}
