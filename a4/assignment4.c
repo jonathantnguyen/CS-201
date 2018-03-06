bedb572d8562442dc16217eb5ce44353d4eadb70
jonathantnguyen <jonnytnguyen@gmail.com>
2018-03-06 12:33:39 -0800

    a

--- a4/assignment4.c
+++ a4/assignment4.c
@@ -4,37 +4,12 @@
 #include <math.h>
 #include <ctype.h>
 
-int _menu();
-
 int main(int argc, char const *argv[])
 {
-	int choice = _menu();
-
-
-
-
-	exit(0);
-
+	if ((argc > 2) || (argv < 16))
+		{
+			printf("[Error] Enter between 2 to 15 arguments.\n");
+			return 0;
+		}
 }
 
-int _menu()
-{
-	int choice = 0;
-
-	printf("Please enter one of the following options by it's corresponding number, and press ENTER\n");
-	printf("\n 0. Exit\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Module\n 6. Reverse Input\n\n");
-
-	scanf(" %i\n", &choice);
-	if(isalpha(choice))
-	{
-		_menu();
-	}
-
-	if ((choice < 0) || (choice > 6))
-	{
-		printf("Invalid input. Try again.\n");
-		_menu();
-	}
-
-	return choice;
-} 
