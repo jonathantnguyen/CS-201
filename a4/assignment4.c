95075f63a2e586fa17252f2eb2b914631a9e5bcb
jonathantnguyen <jonnytnguyen@gmail.com>
2018-03-06 13:46:28 -0800

    a

--- a4/assignment4.c
+++ a4/assignment4.c
@@ -17,7 +17,7 @@ int main(int argc, char const *argv[])
 
 	char * end;
 	char key[] = "0x";
-	for (int i = 0; i < argc; ++i)
+	for (int i = 1; i < argc; ++i)
 	{
 		if (strcmp(key,argv[i]) >= 2)
 		{
