5bf1e3d63c2c1959c725fa528bd1877061121c7b
jonathantnguyen <jonnytnguyen@gmail.com>
2018-03-06 13:37:33 -0800

    a

--- a4/assignment4.c
+++ a4/assignment4.c
@@ -6,10 +6,24 @@
 
 int main(int argc, char const *argv[])
 {
+	int argi[argc];
+
 	if ((argc < 3) || (argc > 16))
 		{
 			printf("[Error] Enter between 2 to 15 arguments.\n");
 			return 0;
 		}
+
+	char * end;
+	char key[] = "0x"
+	for (int i = 0; i < argv; ++i)
+	{
+		if (strcmp(key,*argv[i]) >= 2)
+		{
+			printf("It's hex.\n");
+		}
+		else
+			printf("It's dec.\n");
+	}
 }
 
