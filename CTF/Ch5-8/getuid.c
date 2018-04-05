90e93e8d30b28abcbaf375615501a99096b89889
jonathantnguyen <jonnytnguyen@gmail.com>
2018-04-04 21:42:04 -0700

    a

--- CTF/Ch5-8/getuid.c
+++ CTF/Ch5-8/getuid.c
@@ -9,3 +9,17 @@ int main()
 {
 	return getuid();
 }
+
+
+//BASH
+
+/**
+ * INSTRUCTIONS:
+ * 1. Save as .C file
+ * 
+ * BASH:
+ * 
+ * 2. $ gcc -c -fpic ./getuid.c
+ * 3. $ ld -shared getuid.o - getuid.so
+ * 4. $ LD_PRELOAD=./getuid.so ./Ch7_13_LdPreloadGetUID
+ */
